9569b2cc5de02846837ed4e899aa02cf81c53459
Sumit Mishra <sumitmohanmishra@gmail.com>
2025-11-27 23:38:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
-  import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-  export default defineConfig({
+export default defineConfig({
+  plugins: [react()],
+  base: "/"
+})
+
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
